89ab894fd0283492477824d0ebca8652e71f1cd4
hoàng <chinchin81270@gmail.com>
2026-09-17 15:47:53 +0700

    lab1-b2

--- lab1.js
+++ lab1.js
@@ -1,12 +1,45 @@
 console.log("day la lab 1");
 
-let name = "Thái Hoàng";
-name = "Thái Hoàng";
-const age = 19;
+// Bài 1 - Khai báo biến
+
+let name = "Hoangnt";
+let age = 19;
 let address = "Hà Nội";
 let isStudent = true;
 
-console.log("Họ và Tên: ", name);
+console.log("Họ tên:", name);
 console.log("Tuổi:", age);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
+
+
+// Bài 2 - Thay đổi giá trị biến
+
+let age2 = 19;
+
+console.log(age2);
+
+age2 = 20;
+
+console.log(age2);
+
+
+// Bài 3 - Tính toán với biến
+
+const a = 10;
+const b = 5;
+
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+
+// Bài 4 - Hàm chào hỏi
+
+function sayHello(name) {
+    console.log("Xin chào", name);
+}
+
+sayHello("Hòa");
+sayHello("Dũng");
+sayHello("Hoàng");
