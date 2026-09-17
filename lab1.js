32e3a59695b703f3a1d04d5bd2bbd2bd520593d3
hoàng <chinchin81270@gmail.com>
2026-09-17 15:11:12 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -1,8 +1,12 @@
 console.log("day la lab 1");
 
-let name = "thai hoang";
-name = "hoang81";
+let name = "Thái Hoàng";
+name = "Thái Hoàng";
 const age = 19;
+let address = "Hà Nội";
+let isStudent = true;
 
-console.log("Ten toi la: ", name);
-console.log("Tuoi cua toi la:", age);
+console.log("Họ và Tên: ", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
