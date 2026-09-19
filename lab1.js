341be9b1fcf5369739a896f4af3a9dcebcc46be1
hoàng <chinchin81270@gmail.com>
2026-09-19 15:16:29 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -1,7 +1,6 @@
 console.log("day la lab 1");
 
-// Bài 1 - Khai báo biến
-
+console.log("Bài 1");
 let name = "Hoangnt";
 let age = 19;
 let address = "Hà Nội";
@@ -12,9 +11,7 @@ console.log("Tuổi:", age);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
 
-
-// Bài 2 - Thay đổi giá trị biến
-
+console.log("Bài 2");
 let age2 = 19;
 
 console.log(age2);
@@ -24,22 +21,48 @@ age2 = 20;
 console.log(age2);
 
 
-// Bài 3 - Tính toán với biến
-
+console.log("Bài 3");
 const a = 10;
 const b = 5;
 
+console.log("Tổng:", a + b);
 console.log("Hiệu:", a - b);
 console.log("Tích:", a * b);
 console.log("Thương:", a / b);
 
-
-// Bài 4 - Hàm chào hỏi
-
+console.log("Bài 4");
 function sayHello(name) {
     console.log("Xin chào", name);
+    return 10;
 }
 
 sayHello("Hòa");
 sayHello("Dũng");
 sayHello("Hoàng");
+
+console.log("Bài 5");
+function sum(c, d){
+    tong = c + d;
+    return tong;
+}
+const result = sum(10, 20);
+console.log(result);
+
+console.log("Bài 6");
+function calculateTotal(price, quantity){
+    money = price * quantity;
+    return money;
+}
+const total = calculateTotal(50000, 3);
+console.log(total);
+
+console.log("Bài 7");
+const math = 8;
+const english = 7;
+const javascript = 9;
+function calculateAverage(math, english, javascript){
+    avg = (math + english + javascript) / 3;
+    return avg;
+}
+const average = calculateAverage(8, 7, 9);
+console.log(average);
---
+++ lab2.js
@@ -0,0 +1,16 @@
+// array []
+const student1 = "An";
+const student2 = "Bình";
+const student3 = "Nam";
+
+const students = ["An", "Bình", "Nam"];
+const ids = [1, 5, 23,45, 54];
+const actives = [true, false, 42, "hoangnnt"];
+// danh index: 0, 1, 2
+console.log("ban dau", students);
+students[0] = "hoangnnt";
+console.log("sau thay đổi", students);
+
+// for
+for (let i = 0; i < students.length; i++){
+}
